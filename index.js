df7936e567468a19ae9b5779807d478095642dff
sharifuzzaman24 <private.sharifuzzaman@gmail.com>
2024-12-18 21:16:01 +0600

    created get single job api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -30,6 +30,13 @@ async function run() {
         const cursor = jobsCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobsCollection.findOne(query);
+      res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
